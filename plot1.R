1ec8520d5ccaddfc9a247ab0880baac96148ea5d
dormilon <christian.montes.s@gmail.com>
2015-06-04 22:51:50 -0300

    plot1

---
+++ plot1.R
@@ -0,0 +1,8 @@
+power <- read.table("household_power_consumption.txt", sep = ";", header = TRUE, na.strings = "?",)
+power$Date <- as.Date.factor(power$Date, "%d/%m/%Y")
+power <- subset(power, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
+power$Time <- strptime(power$Time, "%X")
+power$Time <- strftime(power$Time, "%X")
+png(filename = "plot1.png", width = 480, height = 480)
+hist(power$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.off()
